24666c7b1a5030a9c0952c02c2697d0d657d7dcb
D3rpyCat <juliette.desormonts@gmail.com>
2019-12-09 19:47:38 +0100

    création de templates, requêtes clients simples

--- views.py
+++ views.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request
 from flask_pymongo import PyMongo
 
 app = Flask(__name__)
@@ -20,43 +20,61 @@ def home():
 
 @app.route('/user/')
 def user():
-    departments_list = departments.aggregate(
-        [{	"$match": {"dept_no": "d005"}},
-         {
-            "$unwind": {
-                "path": "$all_managers"
-            }}
-         ])
-    employees_names = []
-    for department in departments_list:
-        print(department)
-        results = employees.aggregate(
-            [{
-                "$match": {
-                    "emp_no": department['all_managers']['emp_no']
-                }
-            },
-                {
-                    "$project": {
-                        "last_name": 1,
-                        "first_name": 1
-                    }
-            }
-            ])
-        for result in results:
-            employees_names.append(result)
+    dept_no_list = departments.find({}, {"dept_no": 1})
+    return render_template("user.html", dept_no_list=dept_no_list)
 
-    print(employees_names)
-    return render_template("index.html",
-                           employees_names=employees_names)
+
+@app.route('/user/result/all_managers/')
+def user_result_all_managers():
+    dept_no = request.args.get('dept_no')
+    if dept_no != None:
+        departments_list = departments.aggregate(
+            [{	"$match": {"dept_no": str(dept_no)}},
+             {
+                "$unwind": {
+                    "path": "$all_managers"
+                }}
+             ])
+        employees_names = []
+        for department in departments_list:
+            results = employees.aggregate(
+                [{
+                    "$match": {
+                        "emp_no": department['all_managers']['emp_no']
+                    }
+                },
+                    {
+                        "$project": {
+                            "last_name": 1,
+                            "first_name": 1
+                        }
+                }
+                ])
+            for result in results:
+                employees_names.append(result)
+        return render_template("user_result_all_managers.html", employees_names=employees_names)
+
+
+@app.route('/user/result/salary/')
+def user_result_salary():
+    salaries_list = employees.aggregate(
+        [{	"$match": {"first_name": "Sachin", "last_name": "Tsukuda"}},
+         {	"$unwind": {"path": "$all_salaries"}},
+            {	"$match": {"all_salaries.from_date": "1999/09/03",
+                         "all_salaries.to_date": "2006/09/02"}},
+            {"$project": {"all_salaries.salary": 1.0}}])
+    return render_template("user_result_salary.html", salaries_list=salaries_list)
+
 
 @app.route('/admin/')
 def admin():
     return "Admin view"
 
+
 @app.route('/analyst/')
 def analyst():
     return "Analyst view"
+
 
 if __name__ == "__main__":
     app.run(debug=True)
