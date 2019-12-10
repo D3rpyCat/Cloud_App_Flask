89fd14502530d19352bd0cb3a22da52aa1ead5e2
D3rpyCat <juliette.desormonts@gmail.com>
2019-12-10 16:19:23 +0100

    [WIP] Création de compte, login, logout

--- views.py
+++ views.py
@@ -1,7 +1,19 @@
-from flask import Flask, render_template, request
+from flask import Flask, render_template, request, redirect, url_for
+from flask_login import login_user, current_user,logout_user, LoginManager, login_required
+from flask_login.mixins import UserMixin
 from flask_pymongo import PyMongo
+from flask_wtf import FlaskForm
+from wtforms import StringField, PasswordField
+from wtforms.validators import InputRequired, Length
+from werkzeug.security import generate_password_hash, check_password_hash
+
 
 app = Flask(__name__)
+app.config['SECRET_KEY'] = 'NAM4BwQqes3vc84tThTk'
+
+login_manager=LoginManager()
+login_manager.init_app(app)
+
 mongo_admin = PyMongo(
     app, uri="mongodb://admin:admin@devincimdb1027.westeurope.cloudapp.azure.com:30000/cloud_app")
 mongo_analyst = PyMongo(
@@ -11,12 +23,75 @@ mongo_user = PyMongo(
 
 departments = mongo_admin.db.departments
 employees = mongo_admin.db.employees
+users = mongo_admin.db.users
 
+class User():
+    def __init__(self, pseudo, password):
+        self.pseudo = pseudo
+        self.password = password
+        self.authenticated = False
 
-@app.route('/')
+    def is_active(self):
+        """True, as all users are active."""
+        return True
+
+    def get_id(self):
+        """Return the email address to satisfy Flask-Login's requirements."""
+        return self.pseudo
+
+    def is_authenticated(self):
+        """Return True if the user is authenticated."""
+        return self.authenticated
+
+    def is_anonymous(self):
+        """False, as anonymous users aren't supported."""
+        return False
+
+@login_manager.user_loader
+def load_user(user_id):
+    return users.find_one({"_id":user_id})
+
+class RegForm(FlaskForm):
+    pseudo = StringField('pseudo',  validators=[InputRequired(), Length(max=30)])
+    password = PasswordField('password', validators=[InputRequired(), Length(min=4, max=20)])
+
+@app.route('/home/')
+@login_required
 def home():
-    return "Hello world !"
+    return render_template('home.html', name=current_user.pseudo)
 
+@app.route('/register/', methods=['GET', 'POST'])
+def register():
+    form = RegForm()
+    if request.method == 'POST':
+        if form.validate():
+            existing_user = users.find_one({"pseudo":form.pseudo.data})
+            if existing_user is None:
+                hashpass = generate_password_hash(form.password.data, method='sha256')
+                users.insert_one({"pseudo":form.pseudo.data,"password":hashpass})
+                login_user(User(form.pseudo.data,hashpass))
+                return redirect(url_for('home'))
+    return render_template('register.html', form=form)
+
+@app.route('/login/', methods=['GET', 'POST'])
+def login():
+    if current_user.is_authenticated == True:
+        return redirect(url_for('home'))
+    form = RegForm()
+    if request.method == 'POST':
+        if form.validate():
+            check_user = users.find_one({"pseudo":form.pseudo.data})
+            if check_user:
+                if check_password_hash(check_user['password'], form.password.data):
+                    login_user(User(form.pseudo.data,form.password.data))
+                    return redirect(url_for('home'))
+    return render_template('login.html', form=form)
+
+@app.route('/logout/', methods = ['GET'])
+@login_required
+def logout():
+    logout_user()
+    return redirect(url_for('login'))
 
 @app.route('/user/')
 def user():
