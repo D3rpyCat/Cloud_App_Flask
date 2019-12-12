564577824b37052afb003687369f5c24e1c4ab35
D3rpyCat <juliette.desormonts@gmail.com>
2019-12-12 16:00:24 +0100

    Ajax complété + refonte CSS

--- static/js/home.js
+++ static/js/home.js
@@ -5,22 +5,214 @@ $(function () {
         };
         $.ajax({
             type: 'GET', // define the type of HTTP verb we want to use (POST for our form)
-            url: '/test/', // the url where we want to POST
+            url: '/all_managers/', // the url where we want to POST
             data: formData, // our data object
             dataType: 'json', // what type of data do we expect back from the server
-            encode: true
+            encode: true,
+            beforeSend: function () {
+                $('#form-all-managers button').empty()
+                $('#form-all-managers button').append('<span class="spinner-border spinner-border-sm" role="status" aria-hidden="true"></span><span class="sr-only">Loading...</span>')
+            }
         })
-        // using the done promise callback
-        .done(function (data) {
-            
-            // log data to the console so we can see
-            console.log(data);
-            
-            $('#all-managers-result').empty()
-            data.employees_names.forEach(element => {
-                $('#all-managers-result').append('<li>'+element.first_name+' '+element.last_name+'</li>')
-            });                
-        });
+            // using the done promise callback
+            .done(function (data) {
+
+                // log data to the console so we can see
+                console.log(data);
+
+                $('#all-managers-result').empty()
+                data.employees_names.forEach(element => {
+                    $('#all-managers-result').append('<li>' + element.first_name + ' ' + element.last_name + '</li>')
+                });
+
+                $('#form-all-managers button').remove('span')
+                $('#form-all-managers button').text('Valider')
+            });
         e.preventDefault();
+    });
+
+    $('#form-all-employees').submit(function (e) {
+        var formData = {
+            'dept_no2': $('select[name=dept_no2]').val(),
+            'title': $('select[name=title]').val()
+        };
+        $.ajax({
+            type: 'GET', // define the type of HTTP verb we want to use (POST for our form)
+            url: '/all_employees/', // the url where we want to POST
+            data: formData, // our data object
+            dataType: 'json', // what type of data do we expect back from the server
+            encode: true,
+            beforeSend: function () {
+                $('#all-employees-result').empty()
+                $('#form-all-employees button').empty()
+                $('#form-all-employees button').append('<span class="spinner-border spinner-border-sm" role="status" aria-hidden="true"></span><span class="sr-only">Loading...</span>')
+            }
+        })
+            // using the done promise callback
+            .done(function (data) {
+                // log data to the console so we can see
+                console.log(data);
+                if (data.dept_employees_names.length > 0) {
+                    data.dept_employees_names.forEach(element => {
+                        $('#all-employees-result').append('<li>' + element.first_name + ' ' + element.last_name + '</li>')
+                    });
+                }
+                else {
+                    $('#all-employees-result').append('Pas de résultats')
+                }
+
+                $('#form-all-employees button').remove('span')
+                $('#form-all-employees button').text('Valider')
+            });
+        e.preventDefault();
+    });
+
+    $('#form-all-salaries').submit(function (e) {
+        var formData = {
+            'emp_emp_no': $('input[name=emp_emp_no]').val()
+        };
+        $.ajax({
+            type: 'GET', // define the type of HTTP verb we want to use (POST for our form)
+            url: '/all_salaries/', // the url where we want to POST
+            data: formData, // our data object
+            dataType: 'json', // what type of data do we expect back from the server
+            encode: true,
+            beforeSend: function () {
+                $('#form-all-salaries button').empty()
+                $('#form-all-salaries button').append('<span class="spinner-border spinner-border-sm" role="status" aria-hidden="true"></span><span class="sr-only">Loading...</span>')
+                $('#all-salaries-result').empty()
+            }
+        })
+            // using the done promise callback
+            .done(function (data) {
+                // log data to the console so we can see
+                console.log(data);
+                if (data.labels.length > 0 && data.values.length > 0) {
+                    $('#all-salaries-result').append('<canvas id="all-salaries-bar-chart"></canvas>')
+                    var barChartData = {
+                        labels: data.labels,
+                        datasets: [{
+                            label: 'Salaires',
+                            backgroundColor: randomColor({ hue: 'blue', luminosity: 'light' }),
+                            borderWidth: 1,
+                            data: data.values
+                        }]
+                    };
+                    var ctx = document.getElementById('all-salaries-bar-chart').getContext('2d');
+                    window.myBar = new Chart(ctx, {
+                        type: 'bar',
+                        data: barChartData,
+                        options: {
+                            responsive: true,
+                            legend: {
+                                position: 'top',
+                            },
+                            title: {
+                                display: true,
+                                text: 'Histogramme des salaires par période'
+                            },
+                            tooltips: {
+                                mode: 'index',
+                            }
+                        }
+                    });
+                }
+                else {
+                    $('#all-salaries-result').append('Pas de résultats')
+                }
+                $('#form-all-salaries button').remove('span')
+                $('#form-all-salaries button').text('Valider')
+            });
+        e.preventDefault();
+    });
+
+    $('#form-dept-titles').submit(function (e) {
+        var formData = {
+            'dept_no3': $('select[name=dept_no3]').val(),
+            'from_date_year': $('input[name=from_date_year]').val(),
+            'from_date_month': $('input[name=from_date_month]').val()
+        };
+        $.ajax({
+            type: 'GET', // define the type of HTTP verb we want to use (POST for our form)
+            url: '/dept_titles_date/', // the url where we want to POST
+            data: formData, // our data object
+            dataType: 'json', // what type of data do we expect back from the server
+            encode: true,
+            beforeSend: function () {
+                $('#form-dept-titles button').empty()
+                $('#form-dept-titles button').append('<span class="spinner-border spinner-border-sm" role="status" aria-hidden="true"></span><span class="sr-only">Loading...</span>')
+                $('#dept-titles-result').empty()
+            }
+        })
+            // using the done promise callback
+            .done(function (data) {
+                // log data to the console so we can see
+                console.log(data);
+                if (data.labels.length > 0) {
+                    $('#dept-titles-result').append('<canvas id="dept-titles-bar-chart"></canvas>')
+                    var colors = randomColor({ count: 8, hue: 'blue', luminosity: 'light' })
+
+                    var datasets = []
+                    var count = 0
+                    data.datasets.forEach(function (label) {
+                        var data2 = []
+                        for (var key in data.values) {
+                            data.values[key].forEach(function (value) {
+                                if (value.title == label) {
+                                    data2.push(value.count)
+                                }
+                            })
+                        }
+                        var dataset = {
+                            label: label,
+                            backgroundColor: colors[count],
+                            data: data2
+                        }
+                        count++
+                        datasets.push(dataset)
+                    })
+                    var barChartData = {
+                        labels: data.labels,
+                        datasets: datasets
+                    };
+                    var ctx = document.getElementById('dept-titles-bar-chart').getContext('2d');
+                    window.myBar = new Chart(ctx, {
+                        type: 'bar',
+                        data: barChartData,
+                        options: {
+                            title: {
+                                display: true,
+                                text: 'Histogramme empilé des titres des collaborateurs ayant intégré le département - ' + formData.from_date_year + '/' + formData.from_date_month
+                            },
+                            tooltips: {
+                                mode: 'index',
+                                intersect: false
+                            },
+                            responsive: true,
+                            scales: {
+                                xAxes: [{
+                                    ticks: {
+                                        autoSkip: true
+                                    },
+                                    stacked: true,
+                                }],
+                                yAxes: [{
+                                    stacked: true
+                                }]
+                            }
+                        }
+                    });
+                }
+                else {
+                    $('#dept-titles-result').append('Pas de résultats')
+                }
+                $('#form-dept-titles button').remove('span')
+                $('#form-dept-titles button').text('Valider')
+            });
+        e.preventDefault();
+    });
+
+    $('#disconnect').on('click', function () {
+        window.location.href='/logout/'
     });
 });
--- views.py
+++ views.py
@@ -110,119 +110,15 @@ def default():
 @login_required
 def home():
     if current_user.pseudo == 'user':
-        # all_managers
         dept_no_list = departments.find({}, {"dept_no": 1})
-        if request.method == 'GET' and request.args.get('dept_no') != None:
-            dept_no = request.args.get('dept_no')
-        else:
-            dept_no = departments.find_one({}, {"dept_no": 1})['dept_no']
-        departments_list = departments.aggregate(
-            [{	"$match": {"dept_no": str(dept_no)}},
-             {
-                "$unwind": {
-                    "path": "$all_managers"
-                }}
-             ])
-        employees_names = []
-        for department in departments_list:
-            results = employees.aggregate(
-                [{
-                    "$match": {
-                        "emp_no": department['all_managers']['emp_no']
-                    }
-                },
-                    {
-                        "$project": {
-                            "last_name": 1,
-                            "first_name": 1
-                        }
-                }
-                ])
-            for result in results:
-                employees_names.append(result)
-
-        # all_salaries
-        emp_emp_no = 0
-        if request.method == 'GET' and request.args.get('emp_emp_no') != None:
-            emp_emp_no = request.args.get('emp_emp_no')
-        salaries_list = employees.aggregate(
-            [{	"$match": {"emp_no": int(emp_emp_no)}},
-             {	"$unwind": {"path": "$all_salaries"}},
-                {"$project": {"all_salaries": 1.0}}])
-        labels = []
-        values = []
-        for s in salaries_list:
-            labels.append("["+str(s['all_salaries']['from_date']) +
-                          " - "+str(s['all_salaries']['to_date'])+"]")
-            values.append(s['all_salaries']['salary'])
-        if values != []:
-            max_value = max(values)+100
-        else:
-            max_value = 100000
-
-        # all_employees
         title_list = employees.find().distinct("all_titles.title")
 
-        if request.method == 'GET' and request.args.get('dept_no2') != None and request.args.get('title') != None:
-            dept_no2 = request.args.get('dept_no2')
-            title = request.args.get('title')
-        else:
-            dept_no2 = departments.find_one({}, {"dept_no": 1})['dept_no']
-            title = title_list[0]
-
-        dept_employees_names = []
-        dept_employees = employees.aggregate(
-            [{"$match": {"all_dept.dept_no": str(dept_no2)}},
-             {"$unwind": {"path": "$all_titles"}},
-                {"$match": {"all_titles.title": str(title)}},
-                {"$project": {"first_name": 1.0, "last_name": 1.0}}])
-        for emp in dept_employees:
-            dept_employees_names.append(emp)
-
-        # dept_titles_date
-        if request.method == 'GET' and request.args.get('dept_no3') != None and request.args.get('from_date_year') != None and request.args.get('from_date_month') != None:
-            dept_no3 = request.args.get('dept_no3')
-            from_date_year = request.args.get('from_date_year')
-            from_date_month = request.args.get('from_date_month')
-            if len(from_date_month)==1:
-                from_date_month = "0"+from_date_month
-        else:
-            dept_no3 = departments.find_one({}, {"dept_no": 1})['dept_no']
-            list_dates = employees.find().distinct("all_dept.from_date")[0]
-            from_date_year = list_dates[:4]
-            from_date_month = list_dates[5:7]
-
-        dept_titles_per_date = employees.aggregate(
-            [{	"$unwind": {"path": "$all_titles"}},
-             {	"$unwind": {"path": "$all_dept"}},
-                {	"$match": {"all_dept.dept_no": str(dept_no3), "all_dept.from_date": {
-                    "$regex": str(from_date_year)+"/"+str(from_date_month)}}},
-             {"$group": {"_id": {"from_date": "$all_dept.from_date",
-                                 "title": "$all_titles.title"}, "titleCount": {"$sum": 1}}},
-                {"$group": {
-                    "_id": "$_id.from_date",
-                    "titles": {"$push": {
-                        "title": "$_id.title", "count": "$titleCount"}},
-                    "count": {"$sum": "$titleCount"}}},
-                {"$sort": {"_id": 1}},
-                {"$project": {"titles": 1}}
-             ])
-        stacked_bar_labels = []
-        stacked_bar_values = {}
-        dept_titles_datasets = []
-        for x in dept_titles_per_date:
-            stacked_bar_labels.append(x["_id"])
-            stacked_bar_values[x["_id"]] = x["titles"]
-            for title in x["titles"]:
-                if title['title'] not in dept_titles_datasets:
-                    dept_titles_datasets.append(title['title'])
-        
-
-        return render_template('home.html', name=current_user.pseudo, dept_no_list=list(dept_no_list), employees_names=employees_names, dept_no_chosen=dept_no, emp_emp_no=emp_emp_no, bar_labels=labels, bar_values=values, max=max_value, dept_no_chosen2=dept_no2, title_chosen=title, title_list=list(title_list), dept_employees_names=dept_employees_names, dept_no_chosen3=dept_no3, from_date_year=from_date_year,from_date_month=from_date_month,stacked_bar_labels=stacked_bar_labels, dept_titles_datasets=dept_titles_datasets,stacked_bar_values=stacked_bar_values)
+        return render_template('home.html', name=current_user.pseudo, dept_no_list=list(dept_no_list), title_list=list(title_list))
     if current_user.pseudo == 'admin':
         return render_template('home.html', name=current_user.pseudo)
     if current_user.pseudo == 'analyst':
         return render_template('home.html', name=current_user.pseudo)
+
 
 @app.route('/admin/')
 def admin():
@@ -233,8 +129,9 @@ def admin():
 def analyst():
     return "Analyst view"
 
-@app.route('/test/')
-def test():
+
+@app.route('/all_managers/')
+def all_managers():
     # all_managers
     dept_no_list = departments.find({}, {"dept_no": 1})
     if request.method == 'GET' and request.args.get('dept_no') != None:
@@ -247,7 +144,7 @@ def test():
             "$unwind": {
                 "path": "$all_managers"
             }}
-            ])
+         ])
     employees_names = []
     for department in departments_list:
         results = employees.aggregate(
@@ -265,7 +162,94 @@ def test():
             ])
         for result in results:
             employees_names.append(result)
-    return dumps({'success':True,"employees_names":list(employees_names)}), 200, {'ContentType':'application/json'} 
+
+    return dumps({'success': True, "employees_names": list(employees_names)}), 200, {'ContentType': 'application/json'}
+
+
+@app.route('/all_employees/')
+def all_employees():
+    title_list = employees.find().distinct("all_titles.title")
+
+    if request.method == 'GET' and request.args.get('dept_no2') != None and request.args.get('title') != None:
+        dept_no2 = request.args.get('dept_no2')
+        title = request.args.get('title')
+    else:
+        dept_no2 = departments.find_one({}, {"dept_no": 1})['dept_no']
+        title = title_list[0]
+
+    dept_employees_names = []
+    dept_employees = employees.aggregate(
+        [{"$match": {"all_dept.dept_no": str(dept_no2)}},
+            {"$unwind": {"path": "$all_titles"}},
+            {"$match": {"all_titles.title": str(title)}},
+            {"$project": {"first_name": 1.0, "last_name": 1.0}}])
+    for emp in dept_employees:
+        dept_employees_names.append(emp)
+
+    return dumps({'success': True, "dept_employees_names": list(dept_employees_names)}), 200, {'ContentType': 'application/json'}
+
+
+@app.route('/all_salaries/')
+def all_salaries():
+    emp_emp_no = 0
+    if request.method == 'GET' and request.args.get('emp_emp_no') != None:
+        emp_emp_no = request.args.get('emp_emp_no')
+    salaries_list = employees.aggregate(
+        [{	"$match": {"emp_no": int(emp_emp_no)}},
+            {	"$unwind": {"path": "$all_salaries"}},
+            {"$project": {"all_salaries": 1.0}}])
+    labels = []
+    values = []
+    for s in salaries_list:
+        from_date = str(s['all_salaries']['from_date'])[:4]
+        to_date = str(s['all_salaries']['to_date'])[:4]
+        if to_date=="9999":
+            to_date = "Aujourd'hui"
+        labels.append("["+from_date + " - "+to_date+"]")
+        values.append(s['all_salaries']['salary'])
+
+    return dumps({'success': True, "labels": labels, "values": values}), 200, {'ContentType': 'application/json'}
+
+@app.route('/dept_titles_date/')
+def dept_titles_date():
+    if request.method == 'GET' and request.args.get('dept_no3') != None and request.args.get('from_date_year') != None and request.args.get('from_date_month') != None:
+        dept_no3 = request.args.get('dept_no3')
+        from_date_year = request.args.get('from_date_year')
+        from_date_month = request.args.get('from_date_month')
+        if len(from_date_month) == 1:
+            from_date_month = "0"+from_date_month
+    else:
+        dept_no3 = departments.find_one({}, {"dept_no": 1})['dept_no']
+        list_dates = employees.find().distinct("all_dept.from_date")[0]
+        from_date_year = list_dates[:4]
+        from_date_month = list_dates[5:7]
+
+    dept_titles_per_date = employees.aggregate(
+        [{	"$unwind": {"path": "$all_titles"}},
+            {	"$unwind": {"path": "$all_dept"}},
+            {	"$match": {"all_dept.dept_no": str(dept_no3), "all_dept.from_date": {
+                "$regex": str(from_date_year)+"/"+str(from_date_month)}}},
+            {"$group": {"_id": {"from_date": "$all_dept.from_date",
+                                "title": "$all_titles.title"}, "titleCount": {"$sum": 1}}},
+            {"$group": {
+                "_id": "$_id.from_date",
+                "titles": {"$push": {
+                    "title": "$_id.title", "count": "$titleCount"}},
+                "count": {"$sum": "$titleCount"}}},
+            {"$sort": {"_id": 1}},
+            {"$project": {"titles": 1}}
+            ])
+    labels = []
+    values = {}
+    datasets = []
+    for x in dept_titles_per_date:
+        labels.append(x["_id"])
+        values[x["_id"]] = x["titles"]
+        for title in x["titles"]:
+            if title['title'] not in datasets:
+                datasets.append(title['title'])
+
+    return dumps({'success': True, "labels": labels, "values": values,"datasets":datasets}), 200, {'ContentType': 'application/json'}
 
 if __name__ == "__main__":
     app.run(debug=True)
